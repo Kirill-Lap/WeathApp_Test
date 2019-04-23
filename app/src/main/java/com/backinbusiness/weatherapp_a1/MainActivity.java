462eda3092441dee8f6f61e34dee62c98ed0a169
Kirill Lapardin <lapardin@yanex.ru>
2019-04-23 22:44:13 +0900

    Сделан вывод сообщений LifeCycle в logcat

--- app/src/main/java/com/backinbusiness/weatherapp_a1/MainActivity.java
+++ app/src/main/java/com/backinbusiness/weatherapp_a1/MainActivity.java
@@ -1,23 +1,55 @@
 package com.backinbusiness.weatherapp_a1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"onCreate", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onCreate");
+        findViewById(R.id.toSecondActivityBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Toast.makeText(getApplicationContext(),"onStart", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onStart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Toast.makeText(getApplicationContext(),"onRestart", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"onResume", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onResume");
     }
 }
